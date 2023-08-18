3cbef0898162663838a1e54f64d26cdf9f7a779a
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-18 23:45:35 +0500

    feat: Added form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,17 @@
 import React, {useState} from 'react';
 import Nav from './Components/Nav';
+import Form from './Components/Form';
 import './App.css';
 
 function App() {
 
-  const [text, setText] = useState('');
+  const [mode, setMode] = useState('light');
 
   return (
     <>
     <Nav />
-    
-    <div className="container my-4">
-      <h1>Enter your text</h1>
-      <form>
-  <div className="mb-3">
-    <textarea name="text" id="text" cols="140" rows="10" value={text} onChange={(e)=>setText(e.target.value)}></textarea>
-</div>
-</form>
-<button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ ()=>console.log('c')}>To Upper case</button>
-<button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ ()=>console.log('c')}>To Lower case</button>
-<button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ ()=>console.log('c')}>To Title case</button>
-<button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ ()=>console.log('c')}>To Sentence case</button>
-<button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ ()=>console.log('c')}>To camelCase</button>
-<button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ ()=>console.log('c')}>To PascalCase</button>
-
-<div className='my-2'>
-<button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ ()=>console.log('c')}>To Binary</button>
-<button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ ()=>console.log('c')}>To Morsecode</button>
-</div>
-    </div>
     <div className="container">
-      <h3>Text Stats</h3>
-      <h6>Words: {text.split(' ').length}</h6>
-      <h6>Characters: {text.length}</h6>
+    <Form />
     </div>
     </>
   );
---
+++ src/Components/Form.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+
+export default function Form() {
+
+    const [text, setText] = useState('');
+
+    return (
+        <>
+            <div className="container my-4">
+                <h1>Enter your text</h1>
+                <form>
+                    <div className="mb-3">
+                        <textarea name="text" id="text" cols="140" rows="10" value={text} onChange={(e) => setText(e.target.value)}></textarea>
+                    </div>
+                </form>
+                <button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ () => console.log('c')}>To Upper case</button>
+                <button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ () => console.log('c')}>To Lower case</button>
+                <button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ () => console.log('c')}>To Title case</button>
+                <button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ () => console.log('c')}>To Sentence case</button>
+                <button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ () => console.log('c')}>To camelCase</button>
+                <button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ () => console.log('c')}>To PascalCase</button>
+
+                <div className='my-2'>
+                    <button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ () => console.log('c')}>To Binary</button>
+                    <button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ () => console.log('c')}>To Morsecode</button>
+                </div>
+
+                <div className='my-2'>
+                    <button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ () => console.log('c')}>Clear Text</button>
+                    <button type="button" className="btn btn-primary mx-1" onClick={/*TODO: Implement the handlers */ () => console.log('c')}>Copy Text</button>
+                </div>
+            </div>
+            <div className="container">
+                <h3>Text Stats</h3>
+                <h6>Words: {text.split(' ').length}</h6>
+                <h6>Characters: {text.length}</h6>
+            </div>
+        </>
+    )
+}
