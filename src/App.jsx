813f6a6ad37bd71502e3e4924e637ba5b6b176b1
Muazzam Khan <muazzamkhan1802@icloud.com>
2023-08-19 12:49:41 +0500

    feat: Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Nav from './Components/Nav';
 import Form from './Components/Form';
 import About from './Components/About'
 import Alert from './Components/Alert';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
@@ -24,11 +25,17 @@ function App() {
   return (
 
     <>
-    <Nav mode = {mode} toggle = {setMode} alert = {showAlert}/>
-    <Alert mode= {alertMode}/>
-    <div className="container" style={{display: 'flex', justifyContent: 'center'}}>
-    <Form mode = {mode} alert = {showAlert}/>
-    </div>
+      <Router>
+        <Nav mode={mode} toggle={setMode} alert={showAlert} />
+        <Alert mode={alertMode} />
+        <div className="container" style={{ display: 'flex', justifyContent: 'center' }}>
+          <Routes>
+            <Route path='/' element={<Form mode={mode} alert={showAlert} />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<Form mode={mode} alert={showAlert} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
